2cb2ee48d855c93222ec13a99f7a191b2bfdba95
25838 <2583825704@qq.com>
2021-08-07 16:15:35 +0800

    registration function is completed

--- src/main/java/com/example/community/service/UserService.java
+++ src/main/java/com/example/community/service/UserService.java
@@ -2,16 +2,102 @@ package com.example.community.service;
 
 import com.example.community.dao.UserMapper;
 import com.example.community.entity.User;
+import com.example.community.util.CommunityConstant;
+import com.example.community.util.CommunityUtil;
+import com.example.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
 
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private MailClient mailClient;
+
+    @Value("http://localhost:8080")
+    private String domain;
+
+    @Value("/community")
+    private String contextPath;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
     public User findUserById(int id){
         return userMapper.selectById(id);
+    }
+
+    public Map<String,Object> register(User user){
+        Map<String,Object> map=new HashMap<>();
+
+        if(user==null)
+            throw new IllegalArgumentException("参数不能为空！");
+
+        if(StringUtils.isBlank(user.getUsername())){
+            map.put("usernameMsg","账号不能为空！");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getPassword())){
+            map.put("usernameMsg","密码不能为空！");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "邮箱不能为空!");
+            return map;
+        }
+
+        User u=userMapper.selectByName(user.getUsername());
+        if(u!=null){
+            map.put("usernameMsg","该账号已存在");
+            return map;
+        }
+
+        u=userMapper.selectByEmail(user.getEmail());
+        if(u!=null){
+            map.put("emailMsg","该邮箱已被注册");
+            return map;
+        }
+
+        //注册用户
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5(user.getPassword()+user.getSalt()));
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png",new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        Context context=new Context();
+        context.setVariable("email",user.getEmail());
+        String url=domain+contextPath+"/activation"+"/"+user.getId()+"/"+user.getActivationCode();
+        context.setVariable("url",url);
+        String content=templateEngine.process("/mail/activation",context);
+        mailClient.sendMail(user.getEmail(),"激活账号",content);
+
+        return map;
+    }
+
+    public int activation(int userId,String code){
+        User user=userMapper.selectById(userId);
+        if(user.getStatus()==1)
+            return ACTIVATION_REPEAT;
+        else if(user.getActivationCode().equals(code)){
+            userMapper.updateStatus(userId,1);
+            return ACTIVATION_SUCCESS;
+        } else
+            return ACTIVATION_FAILURE;
     }
 }
--- src/test/java/com/example/community/MapperTests.java
+++ src/test/java/com/example/community/MapperTests.java
@@ -8,6 +8,8 @@ import com.example.community.util.MailClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.util.Date;
 import java.util.List;
@@ -17,6 +19,9 @@ public class MapperTests {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private TemplateEngine templateEngine;
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
@@ -67,5 +72,16 @@ public class MapperTests {
     public void testMail(){
         mailClient.sendMail("ysf981106@163.com","mail test","hello ysf");
         mailClient.sendMail("2583825704@qq.com","mail test","hello ysf");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("ysf981106@163.com", "HTML", content);
     }
 }
