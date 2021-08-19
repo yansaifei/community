470ad7993321ac49a93f74d62a365fb924569139
25838 <2583825704@qq.com>
2021-08-19 17:29:18 +0800

    使用springSecurity实现权限管理

--- src/main/java/com/example/community/controller/interceptor/LoginTicketInterceptor.java
+++ src/main/java/com/example/community/controller/interceptor/LoginTicketInterceptor.java
@@ -8,6 +8,10 @@ import com.example.community.util.HostHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -39,10 +43,15 @@ public class LoginTicketInterceptor implements HandlerInterceptor {
 
             if(loginTicket != null && loginTicket.getStatus() ==0 && loginTicket.getExpired().after(new Date())) {
                 User user = userService.findUserById(loginTicket.getUserId());
+                //本次请求中持有用户
                 hostHolder.setUser(user);
+
+                //将用户认证结果存入SecurityContext,以便授权
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        user, user.getPassword(), userService.getAuthorities(user.getId()));
+                SecurityContextHolder.setContext(new SecurityContextImpl((authentication)));
             }
         }
-
 
         return true;
     }
@@ -58,5 +67,6 @@ public class LoginTicketInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         hostHolder.clear();
+        //SecurityContextHolder.clearContext();
     }
 }
--- src/main/java/com/example/community/util/CommunityConstant.java
+++ src/main/java/com/example/community/util/CommunityConstant.java
@@ -27,5 +27,23 @@ public interface CommunityConstant {
 
     String TOPIC_PUBLISH = "publish";
 
+    String TOPIC_DELETE = "delete";
+
     int SYSTEM_USER_ID = 1;
+
+    /**
+     * 权限: 普通用户
+     */
+    String AUTHORITY_USER = "user";
+
+    /**
+     * 权限: 管理员
+     */
+    String AUTHORITY_ADMIN = "admin";
+
+    /**
+     * 权限: 版主
+     */
+    String AUTHORITY_MODERATOR = "moderator";
+
 }
